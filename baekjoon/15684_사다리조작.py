9fa23a8aa6ad0f1b306f5ce8339bb9387994c585
june <theresej91@gmail>
2019-10-17 20:01:42 +0900

    20191017afternoon

---
+++ baekjoon/15684_사다리조작.py
@@ -0,0 +1,83 @@
+from pprint import pprint
+
+N, M, H = map(int, input().split())     # N은 세로수 갯수(뼈대), M은 가로수 개수(현재), H는 가로선을 놓을수있는 개수
+
+ladder_board = [[0 for j in range(N)] for i in range(H)]
+flag = -1
+
+# pprint(ladder_board)
+
+def downdown(board):
+    # global flag
+    for j in range(N):
+        temp_i = 0
+        temp_j = j
+        while True:
+            if temp_i == H:
+                if temp_j != j:
+                    return False
+                break
+            if board[temp_i][temp_j] == 0:
+                temp_i += 1
+            elif board[temp_i][temp_j] == 1:
+                temp_i += 1
+                temp_j += 1
+            elif board[temp_i][temp_j] == 2:
+                temp_i += 1
+                temp_j -= 1
+    return True
+
+def comb(pick_num, k, arr, now, t_b):
+    global flag
+    if flag != -1:
+        return
+    if k == pick_num:
+        if downdown(t_b) == True:
+            flag = pick_num
+        return 
+    else:
+        for i in range(now + 1, len(candidate)):
+            temp_board = [0 for a in range(H)]
+            for a in range(H):
+                temp_board[a] = t_b[a][:]
+            temp_i, temp_j = candidate[i]
+            if temp_board[temp_i][temp_j] == 0 and temp_board[temp_i][temp_j+1] != 1:
+                temp_board[temp_i][temp_j] = 1
+                temp_board[temp_i][temp_j+1] = 2
+            comb(pick_num, k+1, arr + [candidate[i]], i, temp_board)
+
+
+for i in range(M):
+    temp_i, temp_j = map(int, input().split())
+    ladder_board[temp_i-1][temp_j-1] = 1                # 인덱스 1씩 차이나니까
+    ladder_board[temp_i-1][temp_j] = 2
+
+
+
+candidate = []
+
+for i in range(H):
+    for j in range(N-1):
+        if ladder_board[i][j] == 0 and ladder_board[i][j+1] != 1:
+            candidate.append((i,j))
+
+# print(candidate)
+
+if downdown(ladder_board) == True:
+    flag = 0
+else:
+    for i in range(1,4):
+        # print(str(i) + '번째')
+        comb(i, 0, [], -1, ladder_board)
+
+        if flag != -1:
+            break
+    
+# 가로 사다리 2개 놓기 ==> True반환시 거기서 끝
+    # candidate에서 두개 고르고 downdown 돌리기 ==> 고를때 2개가 서로 간섭되는 관계면 중간에 컷
+
+# 가로 사다리 3개 놓기 ==> True반환시 거기서 끝, 여기서도 안되면 -1출력
+    # candidate에서 세개 고르고 downdown 돌리기 ==> 고를때 3개가 서로 간섭되는 관계면 중간에 컷
+
+
+print(flag)
---
+++ baekjoon/16235_나무재테크.py
@@ -0,0 +1,88 @@
+from pprint import pprint
+from heapq import heappush, heappop
+D = [(0,1), (0,-1), (1,0), (-1,0), (1, 1), (1,-1), (-1,1), (-1,-1)]
+
+
+N, M, K = map(int, input().split())
+
+board = [[[] for j in range(N)] for i in range(N)]
+
+tree_food = [[5 for j in range(N)] for i in range(N)]
+
+A = [0] * N
+
+for i in range(N):              # 각 칸에 추가하는 양분 정보가 저장됨 보드
+    A[i] = list(map(int, input().split()))
+
+
+
+
+for i in range(M):
+    x, y, age = map(int, input().split())
+    board[x-1][y-1].append(age)
+
+pprint(tree_food)
+pprint(board)
+print(A)
+
+year = 0
+current_tree_num = M        # 나무가 죽거나 생길때 바로바로 -1, +1 해준다
+
+while year < K:
+
+    print()
+    print(M)
+    pprint(board)
+    pprint(tree_food)
+    # 봄 + 여름
+    for i in range(N):
+        for j in range(N):
+            flag = 1
+            temp_tree_list = []
+            if board[i][j] == []:
+                continue
+            for _ in range(len(board[i][j])):   # 해당 땅에 있는 나무 그루수만큼 반복
+                if flag == 1:
+                    temp_tree = heappop(board[i][j])            # 현재보고있는 나무의 나이
+                    # print(year)
+                    # print((i,j))
+                    # print(temp_tree)
+                    temp_difference = tree_food[i][j] - temp_tree    # 해당 땅에 남은 영양분 - 지금보고있는 나무나이
+                    if temp_difference >= 0:
+                        tree_food[i][j] -= temp_tree
+                        temp_tree_list.append(temp_tree + 1)   # 양분을 잘 먹고 살아남은 나무는 나이 + 1해서 임시리스트에 저장
+                    else:
+                        flag = -1   # 충분한 양분이 없어서 못먹고 죽는 경우
+                        tree_food[i][j] += temp_tree//2
+                        M -= 1              # 나무가 죽었음
+                if flag == -1:
+                    if board[i][j]:
+                        temp_tree = board[i][j].pop()
+                        tree_food[i][j] += temp_tree//2
+                        M -= 1                  # 나무가 죽었음
+            board[i][j] = temp_tree_list
+
+    # 가을 + 겨울
+    for i in range(N):
+        for j in range(N):
+            tree_food[i][j] += A[i][j]
+            if board[i][j] == []:
+                continue
+            for tree in board[i][j]:
+                if tree % 5 == 0:
+                    for a in range(8):
+                        di, dj = D[a]
+                        ri = i + di
+                        rj = j + dj
+                        if 0 <= ri < N and 0 <= rj < N:
+                            board[ri][rj].append(1)     # 나무가 생김
+                            M += 1
+    
+    year += 1
+    
+pprint(board)
+if M < 0:
+    M = 0
+
+print(M)
+                    
